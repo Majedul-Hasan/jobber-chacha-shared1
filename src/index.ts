0b4eb4a13532f0d4a86201bd7e4266bca9c37aa0
Majedul Hasan <hasanmajedul@gmail.com>
2025-03-04 23:57:02 +0600

    auth.interface

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+export {
+    IAuthPayload,
+    IAuth,
+    IAuthDocument,
+    IAuthResponse, IAuthBuyerMessageDetails, IEmailMessageDetails,
+    ISignInPayload,
+    ISignUpPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxLogout,
+    IAuthUser,
+    IReduxAddAuthUser,
+    IReduxAuthPayload
+} from "./interfaces/auth.interface"
