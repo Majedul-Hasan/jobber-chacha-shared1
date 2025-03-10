e23dc578de943ffa44e068811946ca88694cbef4
Majedul Hasan <hasanmajedul@gmail.com>
2025-03-10 23:46:16 +0600

    error handler

---
+++ src/error-handler.ts
@@ -0,0 +1,46 @@
+import { StatusCodes } from 'http-status-codes';
+
+
+export interface IErrorResponse {
+    message: string;
+    statusCode: number;
+    status: string;
+    comingFrom: string;
+    serializeErrors(): IError
+}
+
+export interface IError {
+    message: string;
+    statusCode: number;
+    status: string;
+    comingFrom: string;
+}
+
+export abstract class CustomError extends Error {
+    abstract statusCode: number;
+    abstract status: string;
+    comingFrom: string;
+
+    constructor(message: string, comingFrom: string) {
+        super(message);
+        this.comingFrom = comingFrom;
+    }
+
+    serializeErrors(): IError {
+        return {
+            message: this.message,
+            statusCode: this.statusCode,
+            status: this.status,
+            comingFrom: this.comingFrom,
+        }
+    }
+}
+export abstract class BadRequestError extends CustomError {
+    statusCode = StatusCodes.BAD_REQUEST;
+    status = "Bad Request";
+
+    constructor(message: string, comingFrom: string) {
+        super(message, comingFrom);
+
+    }
+}
